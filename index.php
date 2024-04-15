5a46f7a60fb080e26ad2fee56b09ef5688a0400a
Andrew Hinkson <adh1056@comcast.net>
2024-04-14 20:53:08 -0400

    added basic functionality of fat-free, Hello World currently displaying.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//turning on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create instance of base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /',function()
+{
+    echo '<h1>Hello World!</h1>';
+
+    //Render a view page
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Run fat-free
+$f3->Run();
