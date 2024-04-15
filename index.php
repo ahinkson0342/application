abc478aabbd4b19311c1c32699000580885a7b2d
Andrew Hinkson <adh1056@comcast.net>
2024-04-14 22:42:19 -0400

    added some page content and a background image, not super happy with the styling yet, but going to work on it

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ $f3 = Base::instance();
 //Define a default route
 $f3->route('GET /',function()
 {
-    echo '<h1>Hello World!</h1>';
+//    echo '<h1>Hello World!</h1>';
 
-    //Render a view page
-//    $view = new Template();
-//    echo $view->render('views/home.html');
+//    Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
